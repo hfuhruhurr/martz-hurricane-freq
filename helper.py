eb3e6053456ff09e9ad450643d145ad96df6b7c3
Doug Devine <dougdevine@gmail.com>
2024-10-05 21:43:54 -0400

    maniacal style matching/understanding

--- helper.py
+++ helper.py
@@ -27,7 +27,7 @@ basin_colors = {
     'northeastpacific': '#4677a3',
     'northatlantic': '#34aa9f',
     'southindian': '#54dacb',
-    'southpacific': '#648412',
+    'southpacific': '#638312',
     'northindian': '#cc9c09',
 }
 
@@ -40,19 +40,33 @@ def make_chart(df):
 
     # Calculate # of stacks needed (one for each year)
     n_stacks = len(years)
+    
+    # Convert pixels x pixels to inches x inches for matplotlib reasons
+    width_pixels, height_pixels = 638, 522  # OG dimensions
+    dpi = 100  # dealer's choice
+    fudge = 1  # easier to do this than to figure out why exact dimensions causes matching weirdness
+    width_inches = width_pixels / dpi + fudge
+    height_inches = height_pixels / dpi
+    
+    # init
+    plt.style.use('default')
 
-    # Initialize stack_bottoms to np.zeros(n_stacks)
+    # Global settings for all text in the figure
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.serif'] = ['Charter', 'Times New Roman', 'Georgia', 'Charter', 'Didot']
+    
+    # Create the figure and an axes 
+    fig, ax = plt.subplots(figsize=(width_inches, height_inches))
+
+    # Build the stacks of bars
+    bar_width = 0.8
     stack_bottoms = np.zeros(n_stacks)
-
-    width = 0.8
-    fig, ax = plt.subplots(figsize=(6, 4.25))
-
-    # Build the stack
+    
     for basin in basin_sort_order:
         p = ax.bar(
             years,
             basin_hurricanes[basin],
-            width=width,
+            width=bar_width,
             label=basin,
             bottom=stack_bottoms,
             color=basin_colors[basin]
@@ -66,31 +80,31 @@ def make_chart(df):
     # Set the major and minor tick frequency
     ax.xaxis.set_major_locator(MultipleLocator(5))
     ax.xaxis.set_minor_locator(MultipleLocator(1))
-    ax.tick_params(labelsize=8)
+    ax.tick_params(labelsize=10)
 
-    # Customize the plot
-    plt.rcParams['font.family'] = 'serif'
-    plt.rcParams['font.serif'] = ['Charter', 'Didot']
-    plt.ylabel('Number of Hurricanes', fontsize=10)
-    plt.title('Global Major Hurricane Frequency\n1980-2023')
+    ax.set_ylabel('Number of Hurricanes', fontsize=12)
+    plt.title('Global Major Hurricane Frequency\n1980-2023', fontsize=14)
 
     # Add a footer
-    plt.subplots_adjust(bottom=0.14)  # won't add this space if nothing uses that space
     footer_text_1 = 'Data source: Colorado State University (CSU) Department of Atmospheric Science'
     footer_text_2 = 'https://tropical.atmos.colostate.edu/Realtime/' 
     footer_text_3 = 'Chart: Chris Martz'
 
-    fig.text(.13, 0.06, footer_text_1, ha='left', va='center', fontsize=7)
-    fig.text(.13, 0.03, footer_text_2, ha='left', va='center', fontsize=7)
-    fig.text(.13, 0, footer_text_3, ha='left', va='center', fontsize=7)
+    fig.text(0.13, 0.06, footer_text_1, ha='left', va='bottom', fontsize=10, fontstyle='italic')
+    fig.text(0.13, 0.03, footer_text_2, ha='left', va='bottom', fontsize=9)
+    fig.text(0.13, 0.00, footer_text_3, ha='left', va='bottom', fontsize=9)
 
+    # plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.14)  # won't add this space if nothing uses that space
+    # plt.margins(x=0)
+    plt.subplots_adjust(bottom=0.14) 
+    
     # Set the legend
     legend = ax.legend(
         ncol=3,
         loc='upper left',
         bbox_to_anchor=(0, 1),
-        fontsize=8,
-        handlelength=1.75
+        fontsize=9,
+        handlelength=2#1.75
     )
 
     # Rewrite the legend labels to human-ify the basin
