2d3d4150a1e6a3d0ce1ea772ed8974756fac0eae
Doug Devine <dougdevine@gmail.com>
2024-10-06 09:55:26 -0400

    italicized footer finally (some fonts don't have italics)

--- helper.py
+++ helper.py
@@ -53,7 +53,7 @@ def make_chart(df):
 
     # Global settings for all text in the figure
     plt.rcParams['font.family'] = 'serif'
-    plt.rcParams['font.serif'] = ['Charter', 'Times New Roman', 'Georgia', 'Charter', 'Didot']
+    # plt.rcParams['font.serif'] = ['Charter', 'Times New Roman', 'Georgia', 'Charter', 'Didot']
     
     # Create the figure and an axes 
     fig, ax = plt.subplots(figsize=(width_inches, height_inches))
@@ -80,19 +80,19 @@ def make_chart(df):
     # Set the major and minor tick frequency
     ax.xaxis.set_major_locator(MultipleLocator(5))
     ax.xaxis.set_minor_locator(MultipleLocator(1))
-    ax.tick_params(labelsize=10)
+    ax.tick_params(labelsize=9)
 
-    ax.set_ylabel('Number of Hurricanes', fontsize=12)
-    plt.title('Global Major Hurricane Frequency\n1980-2023', fontsize=14)
+    plt.title('Global Major Hurricane Frequency\n1980-2023', fontsize=12)
+    ax.set_ylabel('Number of Hurricanes', fontsize=10)
 
     # Add a footer
     footer_text_1 = 'Data source: Colorado State University (CSU) Department of Atmospheric Science'
     footer_text_2 = 'https://tropical.atmos.colostate.edu/Realtime/' 
     footer_text_3 = 'Chart: Chris Martz'
 
-    fig.text(0.12, 0.06, footer_text_1, ha='left', va='bottom', fontsize=10, style='oblique')
-    fig.text(0.12, 0.03, footer_text_2, ha='left', va='bottom', fontsize=9)
-    fig.text(0.12, 0.00, footer_text_3, ha='left', va='bottom', fontsize=9)
+    fig.text(0.12, 0.06, footer_text_1, ha='left', va='bottom', fontsize=8, style='italic')
+    fig.text(0.12, 0.03, footer_text_2, ha='left', va='bottom', fontsize=8, style='italic')
+    fig.text(0.12, 0.00, footer_text_3, ha='left', va='bottom', fontsize=8, style='italic', weight='bold')
 
     # plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.14)  # won't add this space if nothing uses that space
     # plt.margins(x=0)
@@ -103,8 +103,8 @@ def make_chart(df):
         ncol=3,
         loc='upper left',
         bbox_to_anchor=(0, 1),
-        fontsize=9,
-        handlelength=2#1.75
+        fontsize=8,
+        handlelength=2,
     )
 
     # Rewrite the legend labels to human-ify the basin
